48d2a2dc41b691d6cac7ea70da2fc98053a507df
Phuc Thinh <phucthinhmai00@gmail.com>
2025-08-31 20:01:15 +0700

    refactor(doctor): update Doctor model

--- doctors/models.py
+++ doctors/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-from accounts.models import CustomUser  # Liên kết với model người dùng
+from accounts.models import CustomUser  
 
 class Doctor(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='doctor_profile')
     
-    # Các lựa chọn giới tính cho bác sĩ
     GENDER_CHOICES = [
         ('MALE', 'Male'),
         ('FEMALE', 'Female'),
@@ -15,13 +14,12 @@ class Doctor(models.Model):
     bio = models.TextField(blank=True, null=True)  # Tiểu sử
     hospital = models.CharField(max_length=255)  # Bệnh viện công tác
     
-    # Không cần tạo lại phone_number ở đây, ta sẽ lấy nó từ CustomUser
     def __str__(self):
-        return self.user.full_name  # Trả về tên bác sĩ từ CustomUser
+        return self.user.full_name 
     
     @property
     def phone_number(self):
-        return self.user.phone_number  # Trả về phone_number từ CustomUser
+        return self.user.phone_number 
 
     class Meta:
         verbose_name = 'Doctor'
