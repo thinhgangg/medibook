a87502f59f9fab5136e62ba01fba9aa7f23b9984
Phuc Thinh <phucthinhmai00@gmail.com>
2025-08-31 20:01:51 +0700

    feat(patient): add OneToOneField user

--- patients/models.py
+++ patients/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-from accounts.models import CustomUser  # Liên kết với model người dùng
+from accounts.models import CustomUser
 
 class Patient(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='patient_profile')  # Liên kết 1-1 với CustomUser
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='patient_profile')
     
-    # Các lựa chọn giới tính cho bệnh nhân
     GENDER_CHOICES = [
         ('MALE', 'Male'),
         ('FEMALE', 'Female'),
@@ -14,13 +13,12 @@ class Patient(models.Model):
     dob = models.DateField()  # Ngày sinh
     insurance_no = models.CharField(max_length=100)  # Số bảo hiểm
     
-    # Không cần tạo lại phone_number ở đây, ta sẽ lấy nó từ CustomUser
     def __str__(self):
-        return self.user.full_name  # Trả về tên bệnh nhân từ model người dùng
+        return self.user.full_name 
     
     @property
     def phone_number(self):
-        return self.user.phone_number  # Trả về phone_number từ CustomUser
+        return self.user.phone_number
 
     class Meta:
         verbose_name = 'Patient'
