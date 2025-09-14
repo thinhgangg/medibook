5cf416e40972115eca6d0842b5fdb65b39cb42c9
Phuc Thinh <phucthinhmai00@gmail.com>
2025-09-14 09:15:28 +0700

    hotfix: change from cloudinaryfield to imagefield

--- patients/models.py
+++ patients/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from accounts.models import CustomUser
-from cloudinary.models import CloudinaryField
 
 class Patient(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='patient_profile')
@@ -8,7 +7,7 @@ class Patient(models.Model):
     insurance_no = models.CharField(max_length=50, blank=True, null=True)
     occupation = models.CharField(max_length=100, blank=True, null=True)
     
-    profile_picture = CloudinaryField('image', blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='patients/', blank=True, null=True)
 
     def __str__(self):
         return self.user.full_name 
