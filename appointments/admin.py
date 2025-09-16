d4467282edd3e0ddc3030be4f765b2fdb8f07417
Phuc Thinh <phucthinhmai00@gmail.com>
2025-09-16 19:35:09 +0700

    admin: add image preview thumbnails for Doctor, Patient, and AppointmentImage

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
-from .models import Appointment
+from .models import Appointment, AppointmentImage
+from django.utils.html import format_html
 
-admin.site.register(Appointment)
+class AppointmentImageInline(admin.StackedInline):
+    model = AppointmentImage
+    extra = 1
+    readonly_fields = ["preview"]
+
+    def preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" width="200" />', obj.image.url)
+        return "No image"
+    preview.short_description = "Preview"
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    inlines = [AppointmentImageInline]
